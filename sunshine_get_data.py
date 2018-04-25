9134bd1d097928eca1be9b7d602bae4e0ec9f7b2
Vicigel <wzq199001@gmail.com>
2018-04-25 21:53:47 +0800

    use multiprocessing instead of threading

--- sunshine_get_data.py
+++ sunshine_get_data.py
@@ -30,7 +30,6 @@ if date.isoformat(date.today()) not in os.listdir(os.sep.join([curr_dir, 'result
 
 def make_running_script(hosts):
     for item in hosts:
-        print item
         check_path_file = open(os.sep.join([scripts_file_path, 'check_file_path', item[0]]), 'w')
         check_file = open(os.sep.join((scripts_file_path, 'check_path', item[0])), 'w')
         check_path_file.write('#!/bin/sh\n')
@@ -66,7 +65,7 @@ def check_mysql_path(host):
             print stdout.read()
 
         sftp.put(os.sep.join((scripts_file_path, 'check_file_path', hostname)), './xunjian/check_mysql_path.sh')
-        stdin, stdout, stderr = client.exec_command('sh ./xunjian/check_mysql_path.sh')
+        stdin, stdout, stderr = client.exec_command('sh ./xunjian/check_mysql_path.sh', timeout=30)
         if stderr.read().find('no mysqladmin in') > 0:
             print hostname + ' mysql related path is not configured,please configure it manually in ~/.bash_profile'
     except IOError, e:
@@ -82,10 +81,7 @@ def check_mysql_path(host):
 
 
 def check(hosts, logger):
-    print 'hehehe'
     for host in hosts:
-        print host
-        print 'hahahah'
         try:
             hostname, username, password, mysql_user, mysql_pass, mysql_socket = host
             client = paramiko.SSHClient()
@@ -111,55 +107,51 @@ def check(hosts, logger):
             stdin, stdout, stderr = client.exec_command('chmod a+x ./xunjian/xunjian.sh')
             stdout.read()
             stderr.read()
-            stdin, stdout, stderr = client.exec_command('./xunjian/xunjian.sh')
-            print source_file.format(hostname), dest_file_path + '/{0}.txt'.format(hostname)
+            stdin, stdout, stderr = client.exec_command('./xunjian/xunjian.sh', timeout=30)
+            temp_dest_file = dest_file_path + '/{0}.txt'.format(hostname)
+            print source_file.format(hostname), temp_dest_file
             stdout.read()
             stderr.read()
             sftp.get(source_file.format(hostname), dest_file_path + '/{0}.txt'.format(hostname))
 
             sftp.close()
             client.close()
+            with open(temp_dest_file, 'r') as f:
+                temp_flag = 0
+                for line in f:
+                    if line.find('but they were invalid') > 0:
+                        temp_flag = 1
+            if temp_flag == 1:
+                logger.warn(hostname + ' mytuning.pl may execute failed,please check!')
+            else:
+                logger.info(hostname + ' check successfully!')
         except socket.timeout:
-            logger.error(hostname + ' could not be reached!')
+            logger.error(hostname + ' could not be reached, or exec command timeout!')
         except IOError:
             logger.error(hostname + ' has no Permission')
         except paramiko.ssh_exception.AuthenticationException:
             logger.error(hostname + ' authentication failure!')
+        except paramiko.ssh_exception.SSHException:
+            logger.error(hostname + ' No existing session!')
         finally:
             client.close()
 
 
 def multi_check(check_name, hosts_list):
 
-    logger_dict = {}
-    for host_filename, hosts in hosts_list.items():
-        logger = logging.getLogger(host_filename)
-        logger.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
-        file_handler = logging.FileHandler('.log', 'a')
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-        logger_dict[host_filename] = logger
+    logger = multiprocessing.get_logger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
+    file_handler = logging.FileHandler('check.log', 'w')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
     processes = []
-    if check_name == 'check':
-        for host_filename, hosts in hosts_list.items():
-            print host_filename
-
-            process = multiprocessing.Process(target=check, args=[hosts, logger_dict.get(host_filename)], name=host_filename)
-            print host_filename + ' process start'
-            processes.append(process)
-            print host_filename + ' process end'
-
-        for process in processes:
-            process.start()
-
-    if check_name == 'check_mysql_path':
-        for host in hosts_list:
-            process = multiprocessing.Process(target=check, args=[host, logger], name=host[0])
-            processes.append()
-            process.start()
-
+    for host_filename, hosts in hosts_list.items():
+        p = multiprocessing.Process(target=check if check_name == 'check' else check_mysql_path,
+                                    args=(hosts, logger, ))
+        processes.append(p)
+        p.start()
 
 if __name__ == "__main__":
     param = sys.argv
@@ -171,8 +163,8 @@ if __name__ == "__main__":
         lines = f.readlines()
         file_len = len(lines)
 
-    commands.getoutput('split -l ' + ' '.join([str(file_len / 4), host_list_path]))
-    commands.getoutput('mv xa* ' + host_list_dir)
+#    commands.getoutput('split -l ' + ' '.join([str(file_len / 4), host_list_path]))
+#    commands.getoutput('mv xa* ' + host_list_dir)
 
     host_list = {}
     for host_file in os.listdir(host_list_dir):
@@ -180,6 +172,7 @@ if __name__ == "__main__":
             host_list[host_file] = []
             for line in f:
                 host_list[host_file].append(line.replace('\r\n', '').replace('\n', '').split())
-            make_running_script(host_list[host_file])
-            multi_check(param[1], host_list)
+    make_running_script(host_list[host_file])
+
+    multi_check(param[1], host_list)
 
