06f13150fec95de6b29aebcecf000c54262e0c8b
vicigel <wzq199001@gmail.com>
2018-08-25 00:08:17 +0800

    add support for flask_mysqldb config

--- config.py
+++ config.py
@@ -11,6 +11,11 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <wzq199001@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_COMMENTS_PER_PAGE = os.environ.get('FLASKY_COMMENTS_PER_PAGE')
+    MYSQL_HOST = '127.0.0.1'
+    MYSQL_PORT = 5718
+    MYSQL_USER = 'msandbox'
+    MYSQL_PASSWORD = 'msandbox'
+    MYSQL_DB = 'flask'
 
     @staticmethod
     def init_app(app):
